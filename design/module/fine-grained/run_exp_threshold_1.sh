c69fa7debc0b3bd73f0b8ee2b51fba607e18b142
rannnayy <rani.api3939@gmail.com>
2024-06-11 13:26:58 +0000

    final

--- design/module/fine-grained/run_exp_threshold_1.sh
+++ design/module/fine-grained/run_exp_threshold_1.sh
@@ -6,10 +6,10 @@
 
 echo "new nn_clf_ip_based" >> overhead1.csv
 grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}' >> overhead1.csv
-python3 main.py -path /mnt/extra/continuous-training/dataset/test_8_hours -dataset_name profile_v1.feat_v6_ts.readonly.dataset -data_train_duration_min 1 -data_retrain_duration_min 1 -data_eval_duration_min 0.5 -eval_period 0.5 -model_algo nn_clf -model_name nn_clf_1min -dd_algo ip-based -output nn_clf_ip_based
+python3 main.py -path /mnt/extra/continuous-training/dataset/test_8_hours -dataset_name profile_v1.feat_v6_ts.readonly.dataset -data_train_duration_min 1 -data_retrain_duration_min 1 -data_eval_duration_min 1.5 -eval_period 0.5 -model_algo nn_clf -model_name nn_clf_1min -dd_algo ip-based -output nn_clf_ip_based
 grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}' >> overhead1.csv
 
 echo "nn_clf_ks" >> overhead2.csv
 grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}' >> overhead2.csv
-python3 main.py -path /mnt/extra/continuous-training/dataset/test_8_hours -dataset_name profile_v1.feat_v6_ts.readonly.dataset -data_train_duration_min 1 -data_retrain_duration_min 1 -data_eval_duration_min 0.5 -eval_period 0.5 -model_algo nn_clf -model_name nn_clf_1min -dd_algo kolmogorov-smirnov -output nn_clf_ks
+python3 main.py -path /mnt/extra/continuous-training/dataset/test_8_hours -dataset_name profile_v1.feat_v6_ts.readonly.dataset -data_train_duration_min 1 -data_retrain_duration_min 1 -data_eval_duration_min 1.5 -eval_period 0.5 -model_algo nn_clf -model_name nn_clf_1min -dd_algo kolmogorov-smirnov -output nn_clf_ks
 grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}' >> overhead2.csv
